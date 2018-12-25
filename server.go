85478157aa3b9bc46e4fd15bfbe96e1d45c91e30
Alexander <labianca.alex@gmail.com>
2018-12-25 16:25:29 -0700

    cleanpu logs

--- server.go
+++ server.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"net"
 	"os"
 	"strconv"
@@ -18,16 +17,13 @@ func (s *Server) Listen(connectionHandler func(*net.TCPConn)) {
 	l, err := net.Listen("tcp4", address)
 
 	if err != nil {
-		fmt.Println("Could Not listen")
 		os.Exit(1)
 	}
 
 	for {
-		fmt.Println("Listening for connection on at ", s)
 		conn, err := l.Accept()
 
 		if err != nil {
-			fmt.Println("Error Accepting connection")
 			os.Exit(1)
 			return
 		}
@@ -42,8 +38,6 @@ func (s *Server) Listen(connectionHandler func(*net.TCPConn)) {
 func (s *Server) Connect(ip string, port uint16) (*net.TCPConn, error) {
 	p := strconv.Itoa(int(port))
 	addr := ip + ":" + p
-
-	fmt.Println("Connecting ... ", addr)
 
 	conn, err := net.Dial("tcp4", addr)
 	if err != nil {
