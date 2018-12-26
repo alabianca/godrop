81ce41bc5310d94b91d27deb99ebaf027fb0716c
Alexander <labianca.alex@gmail.com>
2018-12-26 09:23:01 -0700

    cleanup

--- godrop.go
+++ godrop.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bufio"
-	"io"
 	"net"
 	"os"
 	"time"
@@ -39,65 +38,6 @@ func (drop *godrop) Connect(ip string, port uint16) (*net.TCPConn, error) {
 	drop.conn = conn
 
 	return conn, nil
-}
-
-func (drop *godrop) Write(data []byte, done bool) {
-	drop.writer.Write(data)
-
-	if done {
-		drop.writer.Flush()
-	}
-
-}
-
-func (drop *godrop) handleConnection(conn *net.TCPConn) {
-	if len(drop.buf) > 0 {
-		conn.Write(drop.buf)
-		conn.Close()
-		return
-	}
-
-	buf := make([]byte, 1024)
-
-	for {
-		n, err := conn.Read(buf)
-		if err != nil {
-			if err == io.EOF {
-				drop.Write(buf[:n], true)
-				break
-			}
-		}
-
-		drop.Write(buf[:n], false)
-	}
-
-}
-
-func (drop *godrop) ReadAll() error {
-	buf := make([]byte, 1024)
-
-	info, _ := os.Stdin.Stat()
-
-	//no stdin
-	if info.Size() <= 0 {
-		return nil
-	}
-
-	for {
-		n, err := drop.reader.Read(buf)
-
-		if err != nil {
-			if err == io.EOF {
-				break
-			} else {
-				return err
-			}
-		}
-
-		drop.buf = append(drop.buf, buf[:n]...)
-
-	}
-	return nil
 }
 
 func NewGodrop(conf config) *godrop {
@@ -189,68 +129,6 @@ func (drop *godrop) NewP2PConn(conf config) *P2PConn {
 	p2pConn := <-quitChan
 
 	return &p2pConn
-
-}
-
-func ScanForPeers(conf config) chan Peer {
-
-	peer := Peer{}
-	quitChan := make(chan Peer)
-
-	mdnsServer, _ := mdns.New()
-
-	mdnsServer.Browse()
-
-	timer := time.NewTicker(time.Duration(3) * time.Second)
-
-	//start out by querying for SRV recorcds
-	queryName := conf.ServiceName
-	queryType := "SRV"
-
-	go func() {
-		//send a query imediately
-		mdnsServer.Query(queryName, "IN", queryType)
-
-		for {
-			select {
-			case packet := <-mdnsServer.QueryChan:
-				responseData, ok := handleQuery(packet, conf.ServiceName, conf.Host)
-
-				if ok {
-					name := packet.Questions[0].Qname
-					anType := packet.Questions[0].Qtype
-					mdnsServer.Respond(name, anType, packet, responseData)
-				}
-			case packet := <-mdnsServer.ResponseChan:
-				//is the response one we care about?
-				ok := handleResponse(packet, conf.ServiceName, conf.Host)
-
-				if ok {
-					//at this point we got a successfull srv record from a peer. Switch the query mode to 'A' records
-					queryName = conf.Host
-					queryType = "A"
-					ip, port := getPeerData(packet.Answers[0])
-
-					if ip != "" {
-						peer.IP = ip
-					}
-					if port != 0 {
-						peer.Port = port
-					}
-				}
-
-				if peer.IP != "" && peer.Port != 0 {
-					timer.Stop()
-					quitChan <- peer
-				}
-
-			case <-timer.C:
-				mdnsServer.Query(queryName, "IN", queryType)
-			}
-		}
-	}()
-
-	return quitChan
 
 }
 
